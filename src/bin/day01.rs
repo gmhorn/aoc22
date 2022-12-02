94508648c2dec498685dce0af05c28d01dab46bb
Gerald Horn <geraldmhorn@gmail.com>
2022-12-01 20:59:28 -0800

    chore: Refactor

---
+++ src/bin/day01.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let lines: Vec<_> = include_str!("../../data/day01.txt").lines().collect();
+    let mut cals: Vec<u32> = lines
+        .split(|l| l.is_empty())
+        .map(|snacks| {
+            snacks.iter().map(|snack| snack.parse::<u32>().unwrap())
+            .sum()
+        })
+        .collect();
+    
+    cals.sort();
+    cals.reverse();
+
+    println!("{}", cals.first().unwrap());
+    println!("{}", cals.iter().take(3).sum::<u32>());
+}
