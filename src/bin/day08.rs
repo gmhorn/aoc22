35f73e1ff9b43deaf77fccc107fe05bddc03603b
ghorn <gerald.horn@sony.com>
2022-12-08 10:18:38 -0800

    feat: Day 8 part 2

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Error, Result};
+use anyhow::{anyhow, Context, Error, Result};
 use aoc22::Timer;
 use std::collections::HashSet;
 use std::str::FromStr;
@@ -6,7 +6,6 @@ use std::str::FromStr;
 fn main() -> Result<()> {
     let timer = Timer::tick();
 
-    // let forest: Forest2= "30373\n25512\n65332\n33549\n35390".parse()?;
     let forest: Forest = include_str!("../../data/day08.txt").parse()?;
 
     let mut visible: HashSet<Tree> = HashSet::new();
@@ -19,8 +18,15 @@ fn main() -> Result<()> {
         visible.extend(Tree::visible(&mut forest.iter_col(col)));
         visible.extend(Tree::visible(&mut forest.iter_col(col).rev()));
     }
-
     println!("{}", visible.len());
+
+    let most_scenic = forest
+        .iter_trees()
+        .map(|tree| forest.scenic_score(&tree))
+        .max()
+        .context("could not calculate most scenic tree")?;
+    println!("{}", most_scenic);
+
     timer.tock();
     Ok(())
 }
@@ -37,7 +43,7 @@ impl Tree {
         Self { x, y, height }
     }
 
-    pub fn visible(trees: &mut impl Iterator<Item = Tree>) -> Vec<Tree> {
+    pub fn visible(trees: &mut impl Iterator<Item = Self>) -> Vec<Tree> {
         let mut answer = vec![];
 
         if let Some(first) = trees.next() {
@@ -54,6 +60,21 @@ impl Tree {
 
         answer
     }
+
+    pub fn count_visible(&self, trees: &mut impl Iterator<Item = Self>) -> usize {
+        // Advance the iterator until we hit ourself
+        trees.take_while(|other| self.ne(other)).for_each(|_| {});
+
+        // Start counting!
+        let mut count = 0;
+        for tree in trees {
+            count += 1;
+            if self.height <= tree.height {
+                break;
+            }
+        }
+        count
+    }
 }
 
 #[derive(Debug)]
@@ -61,6 +82,16 @@ pub struct Forest {
     width: usize,
     height: usize,
     data: Vec<u8>,
+}
+
+// Alias to make signatures a bit easier
+pub trait TreeIter<'a>:
+    DoubleEndedIterator<Item = Tree> + ExactSizeIterator<Item = Tree> + 'a
+{
+}
+impl<'a, T> TreeIter<'a> for T where
+    T: DoubleEndedIterator<Item = Tree> + ExactSizeIterator<Item = Tree> + 'a
+{
 }
 
 impl Forest {
@@ -72,30 +103,29 @@ impl Forest {
         self.height
     }
 
-    pub fn iter_row(&self, row: usize) -> impl DoubleEndedIterator<Item = Tree> + '_ {
-        self.data
-            .iter()
-            .enumerate()
-            .map(|(idx, &height)| {
-                let x = idx % self.width;
-                let y = idx / self.width;
-                Tree::new(x, y, height)
-            })
-            .skip(row * self.width)
-            .take(self.width)
+    pub fn iter_trees(&self) -> impl TreeIter {
+        self.data.iter().enumerate().map(|(idx, &height)| {
+            let x = idx % self.width;
+            let y = idx / self.width;
+            Tree::new(x, y, height)
+        })
     }
 
-    pub fn iter_col(&self, col: usize) -> impl DoubleEndedIterator<Item = Tree> + '_ {
-        self.data
-            .iter()
-            .enumerate()
-            .map(|(idx, &height)| {
-                let x = idx % self.width;
-                let y = idx / self.width;
-                Tree::new(x, y, height)
-            })
-            .skip(col)
-            .step_by(self.width)
+    pub fn iter_row(&self, row: usize) -> impl TreeIter {
+        self.iter_trees().skip(row * self.width).take(self.width())
+    }
+
+    pub fn iter_col(&self, col: usize) -> impl TreeIter {
+        self.iter_trees().skip(col).step_by(self.width)
+    }
+
+    pub fn scenic_score(&self, tree: &Tree) -> usize {
+        let right = tree.count_visible(&mut self.iter_row(tree.y));
+        let left = tree.count_visible(&mut self.iter_row(tree.y).rev());
+        let down = tree.count_visible(&mut self.iter_col(tree.x));
+        let up = tree.count_visible(&mut self.iter_col(tree.x).rev());
+
+        right * left * down * up
     }
 }
 
@@ -125,5 +155,19 @@ impl FromStr for Forest {
         } else {
             Err(anyhow!("Forest must be square!"))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_scenic_score() {
+        let forest: Forest = "30373\n25512\n65332\n33549\n35390".parse().unwrap();
+
+        assert_eq!(4, forest.scenic_score(&Tree::new(2, 1, 5)));
+        assert_eq!(8, forest.scenic_score(&Tree::new(2, 3, 5)));
+        assert_eq!(0, forest.scenic_score(&Tree::new(0, 0, 3)));
     }
 }
