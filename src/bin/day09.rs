005083e4301692a17779de55005a149423fc1d2a
ghorn <gerald.horn@sony.com>
2022-12-09 10:55:11 -0800

    chore: Finish refactoring Day 9

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -19,7 +19,7 @@ fn main() -> Result<()> {
 }
 
 fn count_tail_positions<const KNOTS: usize>(motions: &[Motion]) -> usize {
-    let mut rope = Rope3::<KNOTS>::new();
+    let mut rope = Rope::<KNOTS>::default();
     let mut tail_positions = HashSet::new();
 
     tail_positions.insert(rope.last().copied().unwrap());
@@ -40,26 +40,18 @@ pub struct Position {
     pub y: i32,
 }
 
-pub struct Rope3<const N: usize>([Position; N]);
+pub struct Rope<const N: usize>([Position; N]);
 
-impl<const N: usize> Rope3<N> {
-    pub fn new() -> Self {
-        Self([Position::default(); N])
-    }
-
-    pub fn tail(&self) -> Option<&Position> {
-        self.0.last()
-    }
-
+impl<const N: usize> Rope<N> {
     pub fn step(&mut self, dir: &Direction) {
-        self.0.get_mut(0).map(|head| {
+        if let Some(head) = self.0.first_mut() {
             match dir {
                 Direction::Left => head.x -= 1,
                 Direction::Right => head.x += 1,
                 Direction::Up => head.y += 1,
                 Direction::Down => head.y -= 1,
-            };
-        });
+            }
+        };
 
         self.0.iter_mut().reduce(|lead, follow| {
             Self::update_follower(lead, follow);
@@ -98,85 +90,17 @@ impl<const N: usize> Rope3<N> {
     }
 }
 
-impl<const N: usize> Deref for Rope3<N> {
+impl<const N: usize> Default for Rope<N> {
+    fn default() -> Self {
+        Self([Position::default(); N])
+    }
+}
+
+impl<const N: usize> Deref for Rope<N> {
     type Target = [Position; N];
 
     fn deref(&self) -> &Self::Target {
         &self.0
-    }
-}
-
-#[derive(Debug, Default)]
-pub struct Rope {
-    head: Position,
-    tail: Position,
-}
-
-impl Rope {
-    pub fn head(&self) -> Position {
-        self.head
-    }
-
-    pub fn tail(&self) -> Position {
-        self.tail
-    }
-
-    pub fn step(&mut self, dir: &Direction) {
-        match dir {
-            /*
-               Consider all the possible initial tail positions given a
-               head position (remembering H and T can overlap in center)
-               and then consider the distance after the move
-                 T T T      1 2 3
-                 T H T  --> 0 1 2
-                 T T T      1 2 3
-               Then the only time the tail needs to move is if its in the
-               rightmost column (delta-x >= 2). In all of those cases, the
-               tail moves to the same row as the head and advances left
-               by one.
-            */
-            Direction::Left => {
-                self.head.x -= 1;
-                if self.head.x.abs_diff(self.tail.x) >= 2 {
-                    self.tail.y = self.head.y;
-                    self.tail.x -= 1;
-                }
-            }
-            // Same analysis as above, just mirrored
-            Direction::Right => {
-                self.head.x += 1;
-                if self.head.x.abs_diff(self.tail.x) >= 2 {
-                    self.tail.y = self.head.y;
-                    self.tail.x += 1;
-                }
-            }
-            // Same analysis as above, with x/y reversed
-            Direction::Up => {
-                self.head.y += 1;
-                if self.head.y.abs_diff(self.tail.y) >= 2 {
-                    self.tail.x = self.head.x;
-                    self.tail.y += 1;
-                }
-            }
-            Direction::Down => {
-                self.head.y -= 1;
-                if self.head.y.abs_diff(self.tail.y) >= 2 {
-                    self.tail.x = self.head.x;
-                    self.tail.y -= 1;
-                }
-            }
-        }
-    }
-}
-
-pub struct Motion {
-    pub steps: u32,
-    pub dir: Direction,
-}
-
-impl Motion {
-    pub const fn new(steps: u32, dir: Direction) -> Self {
-        Self { steps, dir }
     }
 }
 
@@ -185,6 +109,11 @@ pub enum Direction {
     Right,
     Up,
     Down,
+}
+
+pub struct Motion {
+    pub steps: u32,
+    pub dir: Direction,
 }
 
 impl FromStr for Motion {
